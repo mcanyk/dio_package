4a6ae8c8184fb1666d428236804f7b0e176086e4
mcanyk <mcanygmrkaya@gmail.com>
2023-02-04 22:14:08 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:dio_package/remote_api.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:<Widget> [
+            ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.green),
+            onPressed: () {
+             Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => const RemoteApi())); 
+            },
+            child: const Text("Remote API"),),
+          ],
+
+        ),  
+      ),
+    );
+  }
+}
